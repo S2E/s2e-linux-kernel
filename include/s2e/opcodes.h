5dd71ec0f4d388c107a8ae97ed99647d0b4363e2
Vitaly Chipounov <vitaly@cyberhaven.io>
2019-05-27 21:48:24 +0200

    s2e/opcodes: removed obsolete opcodes
    Signed-off-by: Vitaly Chipounov <vitaly@cyberhaven.io>

--- include/s2e/opcodes.h
+++ include/s2e/opcodes.h
@@ -35,8 +35,6 @@ extern "C" {
 // clang-format off
 
 #define BASE_S2E_CHECK          0x00
-#define BASE_S2E_ENABLE_SYMBEX  0x01
-#define BASE_S2E_DISABLE_SYMBEX 0x02
 #define BASE_S2E_MAKE_SYMBOLIC  0x03
 #define BASE_S2E_IS_SYMBOLIC    0x04
 #define BASE_S2E_GET_PATH_ID    0x05
